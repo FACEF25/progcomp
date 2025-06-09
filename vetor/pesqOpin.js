f5e03e0e744889b68e7df9539cdcd5f88194eb07
Isadora Chieregato <162230662+isadorachieregato@users.noreply.github.com>
2025-06-09 08:43:44 -0300

    Create pesqOpin.js

---
+++ vetor/pesqOpin.js
@@ -0,0 +1,17 @@
+function pesqOpiniao(){
+let vetor = [];
+let op1 =0 , op2 =0,  op3 =0
+ 
+
+for (let i = 0; i <= 10; i++) {
+     vetor.push(Number(prompt(`Coloque o numero correspondente a sua satisfação do evento (1)Gostou muito; (2)Gostou; (3)Não gostou   :`)));
+}
+for(let i = 0; i <= 10; i++) {
+     if(vet[i] ==1){
+conta1++
+} else if(vet[i] ==2 ){
+conta2++
+}else{
+conta3++ 
+}
+}
